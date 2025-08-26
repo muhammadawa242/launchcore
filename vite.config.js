3b47ee6024e56f9a5a9ee419551a6901a47035b5
muhammadawa242 <muhammadawa242@gmail.com>
2025-08-26 19:53:16 +0500

    Configure for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/launchcore/', 
+  // This is the crucial part!
+  // Set the base path for the build to the root.
+  base: '/',
 })
-
